b5589facef765f5ee46f603c1649d2e93bb92531
AaronYin0514 <562540603@qq.com>
2020-03-24 20:56:48 +0800

    提交脚本

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "删除本地AaronSwift库"
+
+pod repo remove AaronSwift
+
+echo "本地AaronSwift库——已删除"
+
+echo "添加远程仓库到本地"
+
+pod repo add AaronSwift https://github.com/AaronYin0514/AaronSwift.git
+
+echo "添加远程仓库完成"
+
+echo "发布新版本"
+
+pod repo push AaronSwift AaronSwift.podspec --allow-warnings
+
+echo "发布完成"
+
+if [ -d "~/.cocoapods/repos/AaronSwift/Classes/" ]; then
+	echo "包含Class文件夹，正在删除"
+	rm -rf ~/.cocoapods/repos/AaronSwift/Classes
+	echo "删除Class文件夹完成"
+fi
+
+if [ -d "~/.cocoapods/repos/AaronSwift/Assets/" ]; then
+	echo "包含Assets文件夹"
+        rm -rf ~/.cocoapods/repos/AaronSwift/Assets
+	echo "删除Assets文件夹完成"
+fi
+
