502188b4861cb13f8b90c0c2b3d3f7cb3e1b1050
Aaron <yinzhongzheng0514@zto.com>
2020-05-26 17:19:03 +0800

    测试代码

--- Example/AaronSwift/GIFViewController.swift
+++ Example/AaronSwift/GIFViewController.swift
@@ -18,40 +18,55 @@ class GIFViewController: UIViewController {
         view.backgroundColor = .white
         title = "图片解码"
         
-        normalImageDecode()
-//        gifImageDecode()
+        normalImageDecode(index: 0)
     }
     
-    func normalImageDecode() {
-        let url = Bundle.main.url(forResource: "webp_test", withExtension: "webp")
+    func normalImageDecode(index: Int) {
+        let names = ["jpeg_test.jpg", "png_test.png", "tiff_test.tiff", "heic_test.heic"]
+        let url = Bundle.main.url(forResource: names[index], withExtension: nil)
         let data = try! Data(contentsOf: url!)
-        let decoder = ImageDecoder(scale: 0.5)
+        
+        let decoder = ImageDecoder()
         if decoder.decodable(data) {
             if let image = decoder.decode(data) {
                 imageView.image = image
             } else {
-                print("解码失败")
+                print("\(names[index])解码失败")
             }
         } else {
-            print("解码不了")
+            print("\(names[index])解码不了")
         }
+    }
+    
+    func webpImageDecode() {
+        print("webp解码不了")
     }
     
     func gifImageDecode() {
         let url = Bundle.main.url(forResource: "gif2", withExtension: "gif")
         let data = try! Data(contentsOf: url!)
         
-        let decoder = ImageGIFDecoder(scale: 0.5, thumbnailPixelSize: CGSize(width: 100, height: 100))
-        
+        let decoder = ImageGIFDecoder(thumbnailPixelSize: CGSize(width: 100, height: 100))
         if decoder.decodable(data) {
             if let image = decoder.decode(data) {
                 imageView.image = image
             } else {
-                print("解码失败")
+                print("gif解码失败")
             }
         } else {
-            print("解码不了")
+            print("gif解码不了")
         }
     }
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex < 4 {
+            normalImageDecode(index: sender.selectedSegmentIndex)
+        } else if sender.selectedSegmentIndex == 4 {
+            webpImageDecode()
+        } else {
+            gifImageDecode()
+        }
+    }
+    
 
 }
