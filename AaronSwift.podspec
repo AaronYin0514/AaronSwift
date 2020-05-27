0f5b368d5f1c8d14db54a36d29c8df8c0825f44b
Aaron <yinzhongzheng0514@zto.com>
2020-05-27 08:39:43 +0800

    改脚本

--- AaronSwift.podspec
+++ AaronSwift.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AaronSwift'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'Aaron私有Swift公共组件库AaronSwift.'
 
 # This description is used to generate tags and improve search results.
--- release.sh
+++ release.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-echo "删除本地AaronSwift库"
-
-pod repo remove AaronSwift
-
-echo "本地AaronSwift库——已删除"
-
-echo "添加远程仓库到本地"
-
-pod repo add AaronSwift https://github.com/AaronYin0514/AaronSwift.git
-
-echo "添加远程仓库完成"
-
-echo "发布新版本"
-
-pod repo push AaronSwift AaronSwift.podspec --allow-warnings
-
-echo "发布完成"
-
-if [ -d "~/.cocoapods/repos/AaronSwift/AaronSwift/Classes/" ]; then
-	echo "包含Class文件夹，正在删除"
-	rm -rf ~/.cocoapods/repos/AaronSwift/AaronSwift/Classes
-	echo "删除Class文件夹完成"
-fi
-
-if [ -d "~/.cocoapods/repos/AaronSwift/AaronSwift/Assets/" ]; then
-	echo "包含Assets文件夹"
-        rm -rf ~/.cocoapods/repos/AaronSwift/AaronSwift/Assets
-	echo "删除Assets文件夹完成"
-fi
+# pod repo push AaronSwift AaronSwift.podspec --allow-warnings
+pod repo push AaronSpecs AaronSwift.podspec --sources=https://github.com/AaronYin0514/AaronSpecs.git --allow-warnings --verbose
 
